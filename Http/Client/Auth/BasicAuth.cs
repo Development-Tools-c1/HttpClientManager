1c757be70a0226052ab42936ad2ff0ad60af6717
Berkay <Berkay@DESKTOP-ULLAD9F>
2021-01-10 13:34:42 +0300

    -imp BasicAuth impAdd project files.

---
+++ Http/Client/Auth/BasicAuth.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace HttpClientManager.Http.Client.Auth
+{
+    public class BasicAuth
+    {
+        public HttpClient Client { get; set; }
+        private byte[] AuthToken { get; set; }
+        public BasicAuth(string userName, string password, string endPointUrl)
+        {
+            Client = HttpClientFactory.CreateHttpClient(endPointUrl);
+            AuthToken = Encoding.ASCII.GetBytes($"{userName}:{password}");
+            Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(AuthToken));
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));            
+        }
+        public static BasicAuth CreateBasicAuthClient(string userName, string password, string endPointURL)
+        {
+            return new BasicAuth(userName, password, endPointURL);
+        }
+    }
+}
---
+++ Http/Client/HttpClientFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace HttpClientManager.Http.Client
+{
+    public class HttpClientFactory
+    {
+        public HttpClient HttpClient { get; set; }
+        public HttpClientFactory()
+        {
+            HttpClient = new HttpClient();
+        }
+        public static HttpClient CreateHttpClient()
+        => new HttpClientFactory().HttpClient;
+        public static HttpClient CreateHttpClient(string endPointURL)
+        {
+            var httpClient = new HttpClientFactory().HttpClient;
+            httpClient.BaseAddress = new Uri(endPointURL);
+            return httpClient;
+        }
+    }
+
+}
