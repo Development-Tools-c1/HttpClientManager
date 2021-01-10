e07cc074ee25f5f45b2106d67c615024496f6bbe
Berkay <Berkay@DESKTOP-ULLAD9F>
2021-01-10 17:54:03 +0300

    -imp BearerAuth

---
+++ Http/Client/Auth/BearerAuth.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace HttpClientManager.Http.Client.Auth
+{
+    public class BearerAuth
+    {
+        public HttpClient Client { get; set; }
+        public BearerAuth(string endPointUrl, string authToken)
+        {
+            Client = HttpClientFactory.CreateHttpClient(endPointUrl);
+            Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authToken);
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        public static BearerAuth CreateBasicAuthClient(string endPointUrl, string authToken)
+        {
+            return new BearerAuth(endPointUrl, authToken);
+        }
+    }
+}
